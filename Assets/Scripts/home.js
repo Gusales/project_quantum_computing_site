50b92af8995756a531dc5fe71fe0f03455999625
Gustavo Sales <gussales14@gmail.com>
2024-04-25 01:04:39 -0300

    carrosel para desktop finalizado

--- Assets/Scripts/home.js
+++ Assets/Scripts/home.js
@@ -1,16 +1,8 @@
+document.getElementById("handleShowModal").addEventListener('click', handleShowModal)
+
 function handleShowModal() {
   const menuLinks = document.querySelector('.menu__links').classList
 
   menuLinks.contains('hidden') ? menuLinks.replace('hidden', 'show') : menuLinks.replace('show', 'hidden')
 }
 
-const  swiper = new Swiper(".conjunto-equipes", {
-  pagination: {
-    el: ".swiper-pagination",
-    type: "progressbar",
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
